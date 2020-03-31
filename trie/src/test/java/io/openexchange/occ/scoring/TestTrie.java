972da9c8a96b1db8e1b11080ac1fef97490be5b7
andrew_kandzuba <andrey.kandzuba@gmail.com>
2020-03-31 12:39:11 -0500

    trie index implementation

---
+++ trie/src/test/java/io/openexchange/occ/scoring/TestTrie.java
@@ -0,0 +1,67 @@
+package io.openexchange.occ.scoring;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestTrie {
+
+    @Test
+    void indexInsert() {
+        var index = new Trie();
+        index.insert("ABC", 0);
+        index.insert("ABB", 1);
+        index.insert("DBS", 2);
+        index.insert("AAA", 3);
+        index.insert("ZZZ", 4);
+
+        Assertions.assertNotNull(index);
+    }
+
+    @Test
+    void findNode() {
+        String[] arr = {"MARY", "PATRICIA", "LINDA", "BARBARA", "VINCENZO", "SHON", "LYNWOOD", "JERE", "HAI"};
+
+        var index = new Trie();
+        for (int i = 0; i < arr.length; i++) {
+            index.insert(arr[i], i);
+        }
+
+        var n = index.findNode("LINDA");
+
+        Assertions.assertNotNull(n);
+        Assertions.assertEquals(40, n.score());
+    }
+
+    @Test
+    void search() {
+        var index = new Trie();
+        index.insert("ABC", 0);
+        index.insert("ABB", 1);
+        index.insert("DBS", 2);
+        index.insert("AAA", 3);
+        index.insert("ZZZ", 4);
+
+        Assertions.assertTrue(index.search("ABC"));
+        Assertions.assertTrue(index.search("ZZZ"));
+        Assertions.assertFalse(index.search("AA"));
+        Assertions.assertFalse(index.search("AB"));
+        Assertions.assertFalse(index.search("ABCDE"));
+    }
+
+    @Test
+    void searchPrefix() {
+        var index = new Trie();
+        index.insert("ABC", 0);
+        index.insert("ABB", 1);
+        index.insert("DBS", 2);
+        index.insert("AAA", 3);
+        index.insert("ZZZ", 4);
+
+        Assertions.assertTrue(index.searchPrefix("ABC"));
+        Assertions.assertTrue(index.searchPrefix("ZZZ"));
+        Assertions.assertTrue(index.searchPrefix("AA"));
+        Assertions.assertTrue(index.searchPrefix("AB"));
+        Assertions.assertFalse(index.searchPrefix("ABCDE"));
+        Assertions.assertFalse(index.searchPrefix("AE"));
+    }
+}
