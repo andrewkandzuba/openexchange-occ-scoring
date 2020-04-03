0632d4331b8f8bfda8bc9b0ba60e1adaaf751828
andrew_kandzuba <andrey.kandzuba@gmail.com>
2020-04-03 17:09:45 -0500

    do not output secrets into log

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-echo "_OSSRH_JIRA_USERNAME="$_OSSRH_JIRA_USERNAME
-echo "_OSSRH_JIRA_PASSWORD="$_OSSRH_JIRA_PASSWORD_SECRET
-echo "_DOCKER_USERNAME="$_DOCKER_USERNAME
-echo "_DOCKER_PASSWORD_SECRET="$_DOCKER_PASSWORD_SECRET
-
-echo "Deploy for all branches"
-
 gpg --fast-import /root/.gpg/gpg.key
 cp /jar/cli-*.jar cli/target/
 mvn deploy -P sign,build-extras,docker -DskipTests=true --settings cd/mvnsettings.xml
